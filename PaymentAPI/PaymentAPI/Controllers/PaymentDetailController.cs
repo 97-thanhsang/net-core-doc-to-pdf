527d6204259748e07efd578299846b921a588555
Thanh Sang <anhsang31797@example.com>
2024-03-15 14:40:19 +0700

    clean code

--- PaymentAPI/PaymentAPI/Controllers/PaymentDetailController.cs
+++ PaymentAPI/PaymentAPI/Controllers/PaymentDetailController.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Policy;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using PaymentAPI.Models;
 using Spire.Doc;
-using System.IO;
 
 
 namespace PaymentAPI.Controllers
@@ -36,81 +27,6 @@ namespace PaymentAPI.Controllers
         public PaymentDetailController(PaymentDetailContext context)
         {
             _context = context;
-        }
-
-        // GET: api/PaymentDetail
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<PaymentDetail>>> GetPaymentDetails()
-        {
-          if (_context.PaymentDetails == null)
-          {
-              return NotFound();
-          }
-            return await _context.PaymentDetails.ToListAsync();
-        }
-
-        // GET: api/PaymentDetail/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<PaymentDetail>> GetPaymentDetail(int id)
-        {
-          if (_context.PaymentDetails == null)
-          {
-              return NotFound();
-          }
-            var paymentDetail = await _context.PaymentDetails.FindAsync(id);
-
-            if (paymentDetail == null)
-            {
-                return NotFound();
-            }
-
-            return paymentDetail;
-        }
-
-        // PUT: api/PaymentDetail/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPaymentDetail(int id, PaymentDetail paymentDetail)
-        {
-            if (id != paymentDetail.PaymentDetailID)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(paymentDetail).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PaymentDetailExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return Ok(await _context.PaymentDetails.ToListAsync());
-        }
-
-        // POST: api/PaymentDetail
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<PaymentDetail>> PostPaymentDetail(PaymentDetail paymentDetail)
-        {
-          if (_context.PaymentDetails == null)
-          {
-              return Problem("Entity set 'PaymentDetailContext.PaymentDetails'  is null.");
-          }
-            _context.PaymentDetails.Add(paymentDetail);
-            await _context.SaveChangesAsync();
-
-            return Ok(await _context.PaymentDetails.ToListAsync());
         }
 
         [HttpPost("{type}/convert")]
@@ -274,29 +190,5 @@ namespace PaymentAPI.Controllers
             return Ok();
         }
 
-        // DELETE: api/PaymentDetail/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePaymentDetail(int id)
-        {
-            if (_context.PaymentDetails == null)
-            {
-                return NotFound();
-            }
-            var paymentDetail = await _context.PaymentDetails.FindAsync(id);
-            if (paymentDetail == null)
-            {
-                return NotFound();
-            }
-
-            _context.PaymentDetails.Remove(paymentDetail);
-            await _context.SaveChangesAsync();
-
-            return Ok(await _context.PaymentDetails.ToListAsync());
-        }
-
-        private bool PaymentDetailExists(int id)
-        {
-            return (_context.PaymentDetails?.Any(e => e.PaymentDetailID == id)).GetValueOrDefault();
-        }
     }
 }
